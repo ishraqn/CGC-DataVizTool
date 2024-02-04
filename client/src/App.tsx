22b14ddd11096efa5786cac7b8859910a63be60c
Ishraq Md Nazrul <101521628+ishraqn@users.noreply.github.com>
2024-02-03 21:22:58 -0600

    Add user session,routes, compression and Adjust frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 
 const App: React.FC = () => {
   const [message, setMessage] = useState('');
-  const [count, setCount] = useState(0);
   const [geoJsonData, setGeoJsonData] = useState(null); 
 
   useEffect(() => {
-    fetch('/api/init')
+    console.log('Fetching data from the backend...');
+    fetch('/api/v1')
       .then((response) => response.json())
       .then((data) => setMessage(data.message))
       .catch((error) => console.error('Error fetching data:', error));
@@ -16,7 +16,7 @@ const App: React.FC = () => {
 
 
   useEffect(() => {
-    fetch('/api/v1/data-folder/default-2024-02-02T19-04-55.958Z.geojson') // Hardcoded for now 
+    fetch('/api/v1/data-folder/default-simplified.geojson') // Hardcoded for now 
       .then(response => response.json())
       .then(data => {
         setGeoJsonData(data);
@@ -27,12 +27,7 @@ const App: React.FC = () => {
   return (
     <div className="App full-width">
       <header className="App-header">
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount(count + 1)}>
-            count is {count}
-          </button>
-        </div>
+        <h1>CGC DEV TEAM</h1>
         <main>
           <p>🎊Message from the backend: {message}</p>
         </main>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+    plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 });
