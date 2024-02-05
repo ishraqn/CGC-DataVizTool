e6398e1ed5bc4b7f4e5407c44a7ea8ba09e5c056
Ishraq <101521628+ishraqn@users.noreply.github.com>
2024-02-05 03:26:44 -0600

    backend routes updates-more modularity (#10)

---
+++ server/src/controller/fileController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+// handles the file after it has been uploaded (do something with the file after multer middleware has processed it)
+export const fileController = {
+	upload: (req: Request, res: Response) => {
+		// logic to upload a file goes here
+	},
+	// download: (req: Request, res: Response) => {
+};
