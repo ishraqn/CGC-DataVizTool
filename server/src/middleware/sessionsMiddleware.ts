2950446f034ec30bb7fea72170ed9d0e2184c3fa
Ishraq Nazrul <101521628+ishraqn@users.noreply.github.com>
2024-02-05 03:24:25 -0600

    backend routes updates-more modularity

---
+++ server/src/middleware/sessionsMiddleware.ts
@@ -0,0 +1,15 @@
+import session from "express-session";
+
+// express-session cookie configuration for user session management
+export const sessionMiddleware = session({
+	secret: process.env.SECRET_KEY || "secret",
+	resave: false,
+	saveUninitialized: true,
+	//ternary operator to check if the environment is production or development
+	cookie: {
+		secure: process.env.NODE_ENV === "production",
+		httpOnly: true,
+		sameSite: "strict",
+		maxAge: 1000 * 30, // 30 seconds for testing purposes
+	},
+});
