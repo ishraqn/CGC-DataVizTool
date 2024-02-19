64bed6a6354e581eca649e57ade47540976db0dc
ajaynairdev <108850527+ajaynairdev@users.noreply.github.com>
2024-02-19 14:41:09 -0600

    csv to geojson conversion files

---
+++ server/src/controller/csvController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import path from 'path';
+import fs from 'fs/promises';
+import { convertCSVToGeoJSON as convertCsvToGeoJsonUtil } from '../utils/csv2GeojsonUtils';
+
+export const CSVToGeoJSONControl = async (req: Request, res: Response): Promise<void> => {
+    if (!req.file) {
+        res.status(400).send({ message: 'No file reached csv controller' });
+        return; // Explicitly return to stop execution
+    }else{
+
+        const filePath = path.resolve(__dirname, '../data/uploads/temp', req.file.filename);
+
+        try {
+            const geoJson = await convertCsvToGeoJsonUtil(filePath);
+            res.json(geoJson);
+        } catch (err) {
+            const errorMessage = err instanceof Error ? err.message : 'An unknown error occurred';
+            res.status(500).send({ message: 'Error processing CSV file', error: errorMessage });
+            return; // Explicitly return to stop execution
+        }
+    }
+};
