230830ecdab7cc949c1bc86e725e2f2ef234e323
TSM Rodshi <zapscientist@gmail.com>
2024-03-16 23:24:23 -0500

    Fix dynamic zoom

--- client/src/components/geoJSONMap.tsx
+++ client/src/components/geoJSONMap.tsx
@@ -100,7 +100,7 @@ const GeoJSONMap: React.FC<GeoJSONMapProps> = ({ geoJsonData }) => {
                 if (bounds.isValid()) {
                     map.fitBounds(bounds);
                     map.setMaxBounds(bounds);
-                    map.setMinZoom(map.getZoom());
+                    map.setMinZoom(1);
                 }
             }
             if (map.tap) map.tap.disable();
@@ -117,7 +117,7 @@ const GeoJSONMap: React.FC<GeoJSONMapProps> = ({ geoJsonData }) => {
         <>
             <MapContainer
                 key={mapKey}
-                zoom={1}
+                zoom={0}
                 zoomControl={true}
                 keyboard={false}
                 preferCanvas={false}
