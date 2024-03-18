2b47527aa11497ab3d590e7895a1b08b25f19be6
loganDoran <97132002+loganDoran@users.noreply.github.com>
2024-03-18 01:27:20 -0500

    improve guide content

--- client/src/components/GuideTooltip.tsx
+++ client/src/components/GuideTooltip.tsx
@@ -16,14 +16,14 @@ const TooltipContent = () => {
         <li>CSV (Comma-Separated Values)</li>
       </ul>
       <h3>Accepted Types of Geospatial Identification:</h3>
-      <p>Geospatial identification can be represented using one of the following accepted types: latitude/longitude coordinates, CRUID (Crop Region Unique Identifier), or Abbreviated Province and Crop Region. It is essential to maintain consistency in the chosen type throughout the entire dataset.</p>
+      <p>Geospatial identification can be represented using one of the following accepted types: latitude/longitude coordinates, CARUID, or Abbreviated Province and Crop District (CD). It is essential to maintain consistency in the chosen type throughout the entire dataset.</p>
       <blockquote>
         <p><strong>Note:</strong> If you opt for using Crop District (CD), ensure to include the Abbreviated Province as well. For reference regarding expected abbreviations, consult the <a href="https://www.canada.ca/en/revenue-agency/services/tax/businesses/topics/completing-slips-summaries/financial-slips-summaries/return-investment-income-t5/provincial-territorial-codes.html">provincial and territorial codes</a>.</p>
       </blockquote>
       <h3>1. Order of Columns:</h3>
       <p>Arrange the columns in your dataset in the following order:</p>
       <ol>
-        <li><strong>Geospatial Identification</strong>: Place this as the first column in your file. Choose one consistent method (latitude/longitude, CRUID, or Abbreviated Province and Crop Region) for identifying sample locations.</li>
+        <li><strong>Geospatial Identification</strong>: Place this as the first column in your file. Choose one consistent method (latitude/longitude, CARUID, or Abbreviated Province and Crop District) for identifying sample locations.</li>
         <li><strong>Sample Count</strong></li>
         <li><strong>Additional Properties</strong>: Include any relevant additional properties, such as year or station, in subsequent columns.</li>
       </ol>
@@ -39,16 +39,16 @@ const TooltipContent = () => {
           </tr>
         </thead>
       </table>
-      <p><strong>CRUID:</strong></p>
+      <p><strong>CARUID:</strong></p>
       <table>
         <thead>
           <tr>
-            <th>CRUID</th>
+            <th>CARUID</th>
             <th>Samples</th>
           </tr>
         </thead>
       </table>
-      <p><strong>Abbreviated Province and Crop Region:</strong></p>
+      <p><strong>Abbreviated Province and Crop District (CD):</strong></p>
       <table>
         <thead>
           <tr>
@@ -62,7 +62,7 @@ const TooltipContent = () => {
       <h3>3. Null Values:</h3>
       <p>Indicate empty or missing data fields using appropriate null value indicators:</p>
       <ul>
-        <li>Leave cells empty for null values, or use placeholders such as "N/A" or "NULL" to denote missing information.</li>
+        <li>Leave cells empty for null values, or use placeholders such as "ND", "NA", or 0 to denote missing information.</li>
       </ul>
     </>
   );
@@ -73,10 +73,10 @@ const GuideTooltip: React.FC<GuideTooltipProps> = ({ id, children }) => {
     <>
         <div className='GuideTooltip'>
             <a data-tooltip-id={id}
-            data-tooltip-delay-hide={900000}>
+                data-tooltip-delay-hide={0}>
                 {children}
             </a>
-            <Tooltip id={id} clickable style={{  }}>
+            <Tooltip id={id} clickable>
                 <TooltipContent />
             </Tooltip>
         </div>
