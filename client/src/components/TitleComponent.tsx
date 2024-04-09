8aa2b0e2df1a052fcb9352a3b9193ec63f5fb8e0
Ishraq Nazrul <101521628+ishraqn@users.noreply.github.com>
2024-04-09 15:56:10 -0500

    remove title div unless active

--- client/src/components/TitleComponent.tsx
+++ client/src/components/TitleComponent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./TitleComponent.css";
 
 const TitleComponent = ({ title }) => {
+    if (!title) return null;
     return (
         <div className="title">
             <h2>{title}</h2>
